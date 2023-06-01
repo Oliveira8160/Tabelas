aadc8f0ab3c226f1d459198be0085764fb04cd03
Oliveira8160 <gabriel.0812.oliveira@gmail.com>
2023-06-01 00:09:33 -0300

    Atualização 4 - Json mostrando informações

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { client } from '../client';
+import { ClientService } from './../client.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit{
+
+  clients : client[] = [];
+
+  constructor (private ClientService : ClientService){}
+  ngOnInit(): void {
+    this.loadClients();
+  }
+  loadClients() {
+    this.ClientService.getClients().subscribe({
+
+      next : data => this.clients = data
+
+    })
+  }
 
 }
