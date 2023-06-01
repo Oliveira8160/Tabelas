b79945041e3e998aa94205a399f683cc6b6da6c6
Oliveira8160 <gabriel.0812.oliveira@gmail.com>
2023-05-31 23:46:56 -0300

    Atualização 3 - Http e url~

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { client } from './client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getClients () : Observable<client[]>{
+
+    let url = "http://localhost:3000/clients"
+    return this.http.get<client[]>(url);
+  }
 }
